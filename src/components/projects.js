abfc958b05a418a0f2908caa0e5568e95288a45a
Malcolm Peterson <mgpetersn@gmail.com>
2021-09-09 10:28:07 -0400

    reformatted portfolio project list

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,22 +1,113 @@
-export default function Projects(){
-    return (
-        <div className="portfolio_projects">
-            <container className="project_div">
-                <h3>Project1</h3>
-                <p>This project was made to do this thing a few times over and over again. I used these technologies and these...and these and these and ohhh this one too. It took about this long to do and was very fun. It was a small project that I did for fun and maybe for a coding take home project as well. Once again thats a project for this thing only.</p>
-            </container>
-            <container>
-                <h3>Project2</h3>
-                <p>This project was made to do this thing a few times over and over again. I used these technologies and these...and these and these and ohhh this one too. It took about this long to do and was very fun. It was a small project that I did for fun and maybe for a coding take home project as well. Once again thats a project for this thing only.</p>
-            </container>
-            <container>
-                <h3>Project3</h3>
-                <p>This project was made to do this thing a few times over and over again. I used these technologies and these...and these and these and ohhh this one too. It took about this long to do and was very fun. It was a small project that I did for fun and maybe for a coding take home project as well. Once again thats a project for this thing only.</p>
-            </container>
-            <container>
-                <h3>Project4</h3>
-                <p>This project was made to do this thing a few times over and over again. I used these technologies and these...and these and these and ohhh this one too. It took about this long to do and was very fun. It was a small project that I did for fun and maybe for a coding take home project as well. Once again thats a project for this thing only.</p>
-            </container>
+import scrollgif from "./gifs/portpagescroll.gif"
+
+export default function Projects() {
+  return (
+    <div className="portfolio_projects">
+      <container className="project_div">
+        <h3>mPloy Database</h3>
+        <p>
+          This project was made to do this thing a few times over and over
+          again. I used these technologies and these...and these and these and
+          ohhh this one too. It took about this long to do and was very fun. It
+          was a small project that I did for fun and maybe for a coding take
+          home project as well. Once again thats a project for this thing only.
+        </p>
+        <div>
+          <a
+            href="https://github.com/swimtrunks/database-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub |
+          </a>
+          <a href="/"> Try it Out</a>
         </div>
-    )
+      </container>
+      <container>
+        <h3>Personal Page</h3>
+        
+        <center>
+        <img src = {scrollgif} alt="small gif of personal page scrolling"/>
+        </center>
+
+        <p>
+          This project was made to do this thing a few times over and over
+          again. I used these technologies and these...and these and these and
+          ohhh this one too. It took about this long to do and was very fun. It
+          was a small project that I did for fun and maybe for a coding take
+          home project as well. Once again thats a project for this thing only.
+        </p>
+        <div>
+          <a
+            href="https://github.com/swimtrunks/personalpage-malcolm"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub |
+          </a>
+          <a href="/"> Try it Out</a>
+        </div>
+      </container>
+      <container>
+        <h3>Insure Landing Page|Front End Mentor</h3>
+        <p>
+          This project was made to do this thing a few times over and over
+          again. I used these technologies and these...and these and these and
+          ohhh this one too. It took about this long to do and was very fun. It
+          was a small project that I did for fun and maybe for a coding take
+          home project as well. Once again thats a project for this thing only.
+        </p>
+        <div>
+          <a
+            href="https://github.com/swimtrunks/insureFEM"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub |
+          </a>
+          <a href="/"> Try it Out</a>
+        </div>
+      </container>
+      <container>
+        <h3>Malcolm & Tori Wedding</h3>
+        <p>
+          This project was made to do this thing a few times over and over
+          again. I used these technologies and these...and these and these and
+          ohhh this one too. It took about this long to do and was very fun. It
+          was a small project that I did for fun and maybe for a coding take
+          home project as well. Once again thats a project for this thing only.
+        </p>
+        <div>
+          <a
+            href="https://github.com/swimtrunks/malcolmandtori"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub |
+          </a>
+          <a href="/"> Try it Out</a>
+        </div>
+      </container>
+      <container>
+        <h3>wthr| Weather App</h3>
+        <p>
+          This project was made to do this thing a few times over and over
+          again. I used these technologies and these...and these and these and
+          ohhh this one too. It took about this long to do and was very fun. It
+          was a small project that I did for fun and maybe for a coding take
+          home project as well. Once again thats a project for this thing only.
+        </p>
+        <div>
+          <a
+            href="https://github.com/swimtrunks/weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub |
+          </a>
+          <a href="/"> Try it Out</a>
+        </div>
+      </container>
+    </div>
+  );
 }
